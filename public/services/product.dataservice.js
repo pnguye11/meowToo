79a4998481a02ac010139dbe0b43e61b2e40040f
Phuong Nathan Nguyen <pnguye11@gmail.com>
2016-08-25 15:03:27 -0700

    error

--- public/services/product.dataservice.js
+++ public/services/product.dataservice.js
@@ -1,46 +1,46 @@
-(function() {
-  "use strict";
+// (function() {
+//   "use strict";
 
-  angular
-    .module("meowTooApp")
-    .factory("productDataService", productDataService);
+//   angular
+//     .module("meowTooApp")
+//     .factory("productDataService", productDataService);
 
-  productDataService.$inject = ['$http'];
+//   productDataService.$inject = ['$http'];
 
-  function productDataService($http) {
-    var productFactory = {
-      product: {}
-    };
+//   function productDataService($http) {
+//     var productFactory = {
+//       product: {}
+//     };
 
-    // get a single item
-    productFactory.get = function(id) {
-      return $http.get('/api/products/' + id);
-    };
+//     // get a single item
+//     productFactory.get = function(id) {
+//       return $http.get('/api/products/' + id);
+//     };
 
-    // get all products
-    productFactory.all = function() {
-      return $http.get('/api/products/');
-    };
+//     // get all products
+//     productFactory.all = function() {
+//       return $http.get('/api/products/');
+//     };
 
-    // create a item
-    productFactory.create = function(productData) {
-      return $http.post('/api/products/', productData);
-    };
+//     // create a item
+//     productFactory.create = function(productData) {
+//       return $http.post('/api/products/', productData);
+//     };
 
-    // update a item
-    productFactory.update = function(id, productData) {
-      return $http.put('/api/products/' + id, productData);
-    };
+//     // update a item
+//     productFactory.update = function(id, productData) {
+//       return $http.put('/api/products/' + id, productData);
+//     };
 
-    // delete a item
-    productFactory.delete = function(id) {
-      return $http.delete('/api/products/' + id);
-    };
+//     // delete a item
+//     productFactory.delete = function(id) {
+//       return $http.delete('/api/products/' + id);
+//     };
 
 
 
-    // return our entire itemFactory object
-    return productFactory;
-  }
+//     // return our entire itemFactory object
+//     return productFactory;
+//   }
 
-})();
+// })();
